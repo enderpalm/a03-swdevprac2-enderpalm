23013b07a004d4a4fac2f161927f5f5b96adbae6
Palm •-• <ch.enderpalm@gmail.com>
2025-03-12 14:03:12 +0700

    feat: add card and card container in page.tsx, edit body background to be off-white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Banner from "@/components/Banner";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <section className={styles.cardContainer}>
+        <Card />
+      </section>
     </main>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./banner.module.css";
-import React from "react";
 
 export default function Banner(){
   return (
